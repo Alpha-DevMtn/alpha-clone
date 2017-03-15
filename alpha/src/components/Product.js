512c5854d47cf0810f08933f591a603d1a65e4a9
Matt Mecham <math.u@hotmail.com>
2017-03-15 14:40:57 -0600

    yo

--- alpha/src/components/Product.js
+++ alpha/src/components/Product.js
@@ -1,1 +1,39 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { sendToStore } from '../redux/red1';
 
+const bord = {
+   borderStyle: 'solid',
+   width: '210px',
+   height: '310px',
+   margin: '10px',
+   float: 'left'
+}
+
+const imgStyle = {
+  width: '200px'
+}
+
+
+
+class Product extends Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+
+  render() {
+    return (
+      <div style={bord}>
+      <img src={this.props.item.imageurl} alt="" style={imgStyle} />
+      <h3>{this.props.item.prod_name} - {this.props.item.color}</h3>
+      <h3>{this.props.item.price}</h3>
+
+      </div>
+    );
+  }
+}
+
+export default Product;
