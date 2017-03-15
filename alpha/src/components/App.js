90d03ae557c4f04aee84864b82d1cdd7f69e034f
Andrew Bowers <andeb27@gmail.com>
2017-03-15 10:18:37 -0600

    debugging

--- alpha/src/components/App.js
+++ alpha/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import header from '../images/image1.JPG';
 import TakeControl from './TakeControl';
 import Shipping from './Shipping';
 import Popular from './Popular';
